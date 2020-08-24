d37884f213c41e0af3199452b01d921c2bf25029
peilisher <peilisher@gmail.com>
2020-08-24 21:27:44 +0800

    update

--- src/main/java/com/pei/leetcode/Solution.java
+++ src/main/java/com/pei/leetcode/Solution.java
@@ -1,42 +1,14 @@
 package com.pei.leetcode;
 
 
-import java.util.LinkedList;
-
 public
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        LinkedList<int[]> stack = new LinkedList<>();
-        put(stack, newInterval);
-        for (int[] interval : intervals) {
-            put(stack, interval);
-        }
-        int[][] ans = new int[stack.size()][2];
-        for (int i = 0; i < stack.size(); i++) {
-            ans[i] = stack.get(stack.size() - 1 - i);
-        }
-        return ans;
-    }
+    public boolean repeatedSubstringPattern(String s) {
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(0)) {
 
-    void put(LinkedList<int[]> stack, int[] cur) {
-
-        if (stack.isEmpty()) {
-            stack.push(cur);
-        } else {
-            int[] peek = stack.peek();
-            if (cur[0] > peek[1]) {
-                stack.push(cur);
-            } else if (cur[1] < peek[0]) {
-                int[] pop = stack.pop();
-                put(stack, cur);
-                stack.push(pop);
-            } else {
-                int[] pop = stack.pop();
-                int[] tmp = {
-                        Math.min(pop[0], cur[0]), Math.max(pop[1], cur[1])
-                };
-                put(stack, tmp);
             }
         }
+        return true;
     }
 }
